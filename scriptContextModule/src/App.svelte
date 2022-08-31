763e31e83ccd5e4e306f95948245de886ae57d22
ismetozkan <ismetozkan32@hotmail.com>
2022-08-31 14:52:02 +0200

    scriptContextModule

---
+++ scriptContextModule/src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+    import ContMod, {tot} from "./ContMod.svelte";
+
+</script>
+<button on:click={()=>alert(tot())}>Click for Total Value</button>
+<hr>
+<ContMod/>
+<ContMod/>
+<ContMod/>
+<p style="width: 300px;
+          background: aquamarine">
+    &emsp;Even though we have called the
+    same component many times, the
+    <i>'JS'</i> part that we defined with the
+    <b>context="module"</b>
+    only works once. In this way,
+    I was able to change the variables
+    in it with a single component. <br><br>
+    &emsp;If we want to share values to different
+    components we can use <i>stores</i> for that
+    (as it is said in the documentation.)
+</p>
+
+
---
+++ scriptContextModule/src/ContMod.svelte
@@ -0,0 +1,22 @@
+<script context="module">
+    /*Runs once when the module first evaluates,
+    rather than for each component instance. */
+    let totalCount = 0
+    export function tot() {
+        return totalCount;
+    }
+</script>
+
+
+<script>
+    let counter = 0;
+    function addOne() {
+        counter++;
+        totalCount++;
+        console.log(totalCount);
+    }
+</script>
+
+<button on:click={addOne}>Add One to --> ({counter})</button>
+<br>
+
